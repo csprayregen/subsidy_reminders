92f08d40ae96a397ae2d558473cc6450fc2681d2
csprayregen <csprayregen@users.noreply.github.com>
2017-06-09 13:12:03 -0400

    devise authentication added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_provider!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :first_name, :last_name, :business_type])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:username, :first_name, :last_name, :business_type])
+
+  end
+
 
   protect_from_forgery with: :exception
 end
